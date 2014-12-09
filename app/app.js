77a8be49dd3f88e1b62356fb6d09d825c36b467f
Jon Barson <jbarson@gmail.com>
2014-12-09 10:03:36 -0500

    Now creates a hex grid. on the main page.

--- app/app.js
+++ app/app.js
@@ -1,19 +1,33 @@
 (function () {
   'use strict';
 
-  angular.module('myApp', [
-    'ui.router',
-    'myApp.view1',
-    'myApp.view2',
-    'myApp.version'
-  ]).controller('Main',function(){
+  angular.module('myApp', [])
+    .controller('Main',mainCtrl);
+
+    function mainCtrl(){
       var main = this;
-      main.hexes =[];
-      for (var j=0;j<50;j++) {
-        for (var i = 0; i < 50; i++) {
-          main.hexes.push([i,j]);
-        }
+      main.hexes = hexes();
+      main.hexNumber = hexNumber;
+      main.translation = translation;
+      function hexes(){
+        var hexNumbers =[];
+        for (var j=1;j<61;j++) {
+          for (var i = 1; i < 56; i++) {
+            hexNumbers.push([i,j]);}}
+        return hexNumbers;
       }
-    })
+      function hexNumber(hex){
+        //takes a hex (pair of integers)and outputs a standard hex number
+        var pad = function(num){
+          return num.toString().length===1?'0'+num:num;
+        };
+        return pad(hex[0]).toString()+pad(hex[1]).toString();
+      }
+      function translation(hex){
+        //takes a hex and gives a translation string for it's positioning in an SVG
+        return (20+hex[0]*15)+","+(hex[0]%2?hex[1]*17.32-8.66+20:hex[1]*17.32+20);
+      }
+    }
+
 
 })();
